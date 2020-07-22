ed30024910ec57370c41c19d8141e1a638d762a3
Ahetesum <Jsingh@Js-MacBook-Pro.local>
2020-07-22 15:17:26 +0530

    mongo db setup completed

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
-const bodyParser= require("body-parser");
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
+const app = express();
+
+mongoose.connect("mongodb+srv://DatabaseUser:9916708968@cluster0.rymui.mongodb.net/zoomData?retryWrites=true&w=majority").then(()=>{
+console.log("Connected to Database");
+})
+.catch(()=>{
+  console.log("Connection Failed Database");
+});
+const bodyParser= require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -15,7 +24,11 @@ app.use((req,res,next)=>{
 })
 
 app.post('/api/posts',(req,res,next)=>{
-  const post = req.body;
+  const post = new Post({
+    title:req.body.title,
+    description:req.body.description
+  });
+  post.save();
   console.log(post);
   res.status(201).json({
     message:'Post Created Sucessfully'
@@ -25,15 +38,15 @@ app.post('/api/posts',(req,res,next)=>{
 
 app.get('/api/posts',(req,res,next)=>{
 
-  const posts=[
-    {id:"qwe124123eqw",title:"First Post",description:"This is coming from server"},
-    {id:"fdg234324ret",title:'Second Post',description:"this is also coming from server"}
-  ];
+  Post.find().then((documents)=>{
+    console.log(documents);
+    res.status(200).json({
+      message:"Post fetched sucessfully",
+      posts:documents
+    })
+  });
 
-  res.status(200).json({
-    message:"Post fetched sucessfully",
-    posts:posts
-  })
+
 });
 
 module.exports= app;
